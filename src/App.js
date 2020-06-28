d9af5cd8359cf43b370108a3ce2fed67ef9ba9bf
Loh Jing Zhi <jingzhi.loh@skymind.my>
2020-06-29 07:00:44 +0800

    media quesries with Radium

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
   state = 
@@ -119,14 +119,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I am a react app</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Person</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I am a react app</h1>
+          <p className={classes.join(' ')}>This is really working</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Person</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
     
